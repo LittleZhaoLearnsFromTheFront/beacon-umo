cb70559dc769898d22a3369f2d22637517d5bc1d
zhaohx <zhaohx0409@163.com>
2025-01-06 09:36:57 +0800

    feat:增加路由 守卫

--- beacon-umo-server/src/modules/auth/auth.controller.ts
+++ beacon-umo-server/src/modules/auth/auth.controller.ts
@@ -2,13 +2,17 @@ import { Body, Controller, HttpCode, Post, UseGuards } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { LoginDto } from "./dto/login.dto";
 import Result from "@/common/result/Result";
+import { Public } from "@/common/metadata/public.metadata";
+import { JwtGuard } from "@/common/guard/jwt.guard";
 
+@UseGuards(JwtGuard)
 @Controller()
 export class AuthController {
     constructor(
         private readonly authService: AuthService,
     ) { }
-    
+
+    @Public()
     @HttpCode(200)
     @Post('login')
     async login(@Body() body: LoginDto) {
